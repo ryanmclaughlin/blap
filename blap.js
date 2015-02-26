e4359ae9267135dd9ac7a51bc87d285bb9688a2c
Ryan McLaughlin <ryanm@substantial.com>
2015-02-25 16:39:40 -0800

    Added some functions for rendering video from inpt

--- blap.js
+++ blap.js
@@ -1,3 +1,4 @@
+
 if (Meteor.isClient) {
 
   Template.formUpload.helpers({
@@ -10,10 +11,42 @@ if (Meteor.isClient) {
     'submit .form-converter': function (event) {
 
 
+    },
+
+    'change input[type="file"]': function (event) {
+
     }
+
   });
 }
 
+
+function humanFileSize(bytes, si) {
+  var thresh = si ? 1000 : 1024;
+  if(bytes < thresh) return bytes + ' B';
+  var units = si ? ['kB','MB','GB','TB','PB','EB','ZB','YB'] : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+  var u = -1;
+  do {
+      bytes /= thresh;
+      ++u;
+  } while(bytes >= thresh);
+  return bytes.toFixed(1)+' '+units[u];
+}
+
+
+function renderVideo(file){
+  var reader = new FileReader();
+  reader.onload = function(event){
+    the_url = event.target.result
+    $('#data-vid').html("<video width='400' controls><source id='vid-source' src='"+the_url+"' type='video/mp4'></video>")
+     $('#name-vid').html(file.name)
+    $('#size-vid').html(humanFileSize(file.size, "MB"))
+    $('#type-vid').html(file.type)
+
+  }
+
+  reader.readAsDataURL(file);
+}
 
 
 if (Meteor.isServer) {
