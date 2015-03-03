9c85c2fa827ebb02de12b7068cd0612a0a6fb17d
Ryan McLaughlin <mclaughlinry@gmail.com>
2015-03-02 21:33:36 -0800

    Changed to post from get

--- server/savedeo.js
+++ server/savedeo.js
@@ -4,9 +4,11 @@ var api_endpoint = 'https://savedeo.p.mashape.com/download';
 Meteor.methods({
 
   saveDeo: function (url_link) {
-    var response = HTTP.get(api_endpoint, {
+    var response = HTTP.post(api_endpoint, {
       headers: {
-        'X-Mashape-Key': api_key
+        'X-Mashape-Key': api_key,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
       },
       params: {
         url: url_link
