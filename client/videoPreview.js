378b08689f6364338cd3c81f72a3087b43651d1a
Ryan McLaughlin <ryanm@substantial.com>
2015-03-24 17:20:45 -0700

    Add nouislider

--- client/videoPreview.js
+++ client/videoPreview.js
@@ -1,5 +1,18 @@
 Session.setDefault('videoPreviewSource', false);
 
+Template.videoPreview.rendered = function () {
+
+  $('.slider').noUiSlider({
+    start: [20, 80],
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 100
+    }
+  });
+
+};
+
 Template.videoPreview.helpers({
   videoPreviewSource: function () {
     return Session.get('videoPreviewSource');
