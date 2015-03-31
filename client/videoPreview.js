50fd8fa208244e20a92919ddae1e05c86da8e248
Ryan McLaughlin <ryanm@substantial.com>
2015-03-30 18:13:40 -0700

    Wire up slider
    TODO: wire number inputs to update slider

--- client/videoPreview.js
+++ client/videoPreview.js
@@ -1,35 +1,86 @@
+
+var videoDuration;
+var videoStartTime = 0;
+var videoEndTime = 5;
+
 Session.setDefault('videoPreviewSource', false);
+Session.setDefault('videoStartTime', videoStartTime);
+Session.setDefault('videoEndTime', videoEndTime);
 
-Template.videoPreview.rendered = function () {
 
-  $('.slider').noUiSlider({
-    start: [0, 20],
+// Template.videoPreview.rendered = function () {
+// };
+
+function updateVideoCurrentTime (value) {
+  var $this = $(this);
+  $this.val(value);
+
+  $('.video').get(0).currentTime = value;
+
+  if ($this.hasClass('convert-form__trim-from')) {
+    Session.set('videoStartTime', value);
+    console.log(Session.get('videoStartTime'));
+  } else {
+    Session.set('videoEndTime', value);
+    console.log(Session.get('videoEndTime'));
+  }
+};
+
+
+var initSlider = function () {
+  var slider = $('.slider');
+
+  slider.noUiSlider({
+    start: [videoStartTime, videoEndTime],
     connect: true,
     range: {
       'min': 0,
-      'max': 100
+      'max': videoDuration
     }
   });
 
-  $(".slider").on({
-    slide: function (event){
-      //console.log(event)
-    }
-  });
+  // slider.on('slide')
 
+  slider.Link('lower').to($('.convert-form__trim-from'), updateVideoCurrentTime, wNumb({
+    decimals: 1
+  }));
+
+  slider.Link('upper').to($('.convert-form__trim-to'), updateVideoCurrentTime, wNumb({
+    decimals: 1
+  }));
 };
+
 
 Template.videoPreview.events({
   'loadedmetadata video': function (event) {
     var video = event.target;
-    var videoDuration = video.duration;
-    console.log(videoDuration);
+    // Round to two decimal places
+    videoDuration = Math.floor(video.duration * 100) / 100;
+    initSlider();
+  },
+
+  'mouseover video': function (event) {
+    var video = event.target;
+    video.setAttribute('controls', 'controls');
+  },
+
+  'mouseout video': function (event) {
+    var video = event.target;
+    video.removeAttribute('controls')
+  },
+
+  'timeupdate video': function (event) {
+    var video = event.target;
+    if (!video.paused && video.currentTime >= Session.get('videoEndTime')) {
+      console.log('HEYY', Session.get('videoStartTime'), Session.get('videoEndTime'));
+      video.currentTime = Session.get('videoStartTime');
+    }
   }
+
 });
 
 Template.videoPreview.helpers({
   videoPreviewSource: function () {
     return Session.get('videoPreviewSource');
   }
-
 });
